09ca0a3b918fec0310ea02f4f2df6b9fb7e35469
mbrus <brus.michal@outlook.com>
2023-07-26 19:32:50 +0200

    variables names update

--- run_tds.py
+++ run_tds.py
@@ -1,5 +1,5 @@
 import transaction_data_simulator as tds
 tds = tds.TransactionDataSimulator(5, 5)
 
-print(tds.generate_customer_profiles_table())
-print(tds.generate_terminal_profiles_table())
+print(tds.generate_customers_table())
+print(tds.generate_terminals_table())
--- transaction_data_simulator.py
+++ transaction_data_simulator.py
@@ -7,37 +7,39 @@ class TransactionDataSimulator:
         self.number_of_customers = number_of_customers
         self.number_of_terminals = number_of_terminals
         self.random_state = 0
+        self.customers_table = None
+        self.terminals_table = None
 
-    def generate_customer_profiles_table(self):
+    def generate_customers_table(self):
         np.random.seed(self.random_state)
-        customer_id_properties = []
+        customers = []
 
         for customer_id in range(self.number_of_customers):
-            x_customer_id = np.random.uniform(0, 100)
-            y_customer_id = np.random.uniform(0, 100)
+            x_customer = np.random.uniform(0, 100)
+            y_customer = np.random.uniform(0, 100)
 
             mean_amount = np.random.uniform(5, 100)
             std_amount = mean_amount / 2
 
             mean_number_of_tx_per_day = np.random.uniform(0, 4)
-            customer_id_properties.append(
-                [customer_id, x_customer_id, y_customer_id, mean_amount, std_amount, mean_number_of_tx_per_day])
+            customers.append(
+                [customer_id, x_customer, y_customer, mean_amount, std_amount, mean_number_of_tx_per_day])
 
-        customer_profiles_table = pd.DataFrame(customer_id_properties,
-                                               columns=['CUSTOMER_ID', 'x_customer_id', 'y_customer_id',
-                                                        'mean_amount', 'std_amount', 'mean_number_of_tx_per_day'])
-        return customer_profiles_table
+        self.customers_table = pd.DataFrame(customers,
+                                            columns=['CUSTOMER_ID', 'x_customer', 'y_customer',
+                                                     'mean_amount', 'std_amount', 'mean_number_of_tx_per_day'])
+        return self.customers_table
 
-    def generate_terminal_profiles_table(self):
+    def generate_terminals_table(self):
         np.random.seed(self.random_state)
-        terminal_id_properties = []
+        terminals = []
 
         for terminal_id in range(self.number_of_terminals):
-            x_terminal_id = np.random.uniform(0, 100)
-            y_terminal_id = np.random.uniform(0, 100)
+            x_terminal = np.random.uniform(0, 100)
+            y_terminal = np.random.uniform(0, 100)
 
-            terminal_id_properties.append([terminal_id, x_terminal_id, y_terminal_id])
+            terminals.append([terminal_id, x_terminal, y_terminal])
 
-        terminal_profiles_table = pd.DataFrame(terminal_id_properties,
-                                               columns=['TERMINAL_ID', 'x_terminal_id', 'y_terminal_id'])
-        return terminal_profiles_table
+        self.terminals_table = pd.DataFrame(terminals,
+                                            columns=['TERMINAL_ID', 'x_terminal', 'y_terminal'])
+        return self.terminals_table
